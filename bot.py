6a903823f454ba91cc9031e1be27fec8efc5fd45
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2023-10-30 12:32:23 +0530

    Update bot.py
    bla

--- bot.py
+++ bot.py
@@ -14,6 +14,14 @@ Helper functions in the db
 
 is_registered(telegram_id: int) => check if the user with the given id is registered in the database
 
+# not now for the MVP
+get_current_state(telegram_id: int) => get the current state of the user, right now the plan is it to be 
+['registration', 'started'], ['registration', 'name'] (name is to be entered), ['registration', 'regno'], ['registration', 'phone'],
+['registration', 'vitmail'], ['registration', 'hostel block'], ['registration', 'room number']
+
+# for the mvp
+is_registering(telegram_id: int) => [true, false]
+db.store_user_details(message.text: str) => to store the user acc details for new registration
 """
 
 @bot.message_handler(commands = ['start'])
@@ -23,6 +31,20 @@ def start(message):
     else:
         bot.reply_to(message, "welcome to healthbot, we are here to help you out :)")
 
-@bot.messag_handler(commands = ['register'])
+@bot.message_handler(commands = ['register'])
 def register(message):
-    bot.reply_to(message, "Welcome, we'll register right now")
+    if db.is_registered(message.id):
+        bot.reply_to(message, "You are already registed. You do not have to register again")
+    else:
+        bot.reply_to(message, "Enter your name, registration number, phone, vit mail id, room number and block (A, B, C, D1, D2) on seperate lines")
+
+bot.message_handler(func = lambda message: True)
+def router(message):
+    """
+    This function routes the text messages appropriately - acts as a state machine
+    """
+    if db.is_registering(message.id):
+        # this response is then the name, reg, phone thing .., so plug it into db
+        db.store_user_details(message.text)
+
+bot.infinity_polling()
