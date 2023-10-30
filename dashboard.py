5e96b6e4063186b17e361617963df1915980c886
Robin Roy <115863770+robinroy03@users.noreply.github.com>
2023-10-30 21:12:56 +0530

    Update dashboard.py
    meow

--- dashboard.py
+++ dashboard.py
@@ -4,22 +4,18 @@ The streamlit UI for the health center personnels
 
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
-from streamlit_modal import Modal
-
-import streamlit.components.v1 as components
-
 
 import db
 
 st_autorefresh(2000)
 
-def appointment_over(telegram_id: int):
+def appointment_over(telegram_id: int, prescription: str):
     """
-    A wrappper for db.close_appointment() and for showing the modal message and take inputs such as
-    symptoms, medicines, number of days to take medicines, when all (mrng, evng, night)
+    A wrappper for db.close_appointment()
+    takes inputs such as symptoms, medicines, number of days to take medicines, when all (mrng, evng, night)
     """
-    
-
+    print(prescription)
+    db.close_appointment(telegram_id)
 
 for appointment in db.get_active_appointments():
     with st.container():
@@ -28,4 +24,10 @@ for appointment in db.get_active_appointments():
             st.write("Name:", appointment['name'])
             st.write("Age:", appointment['age'])
         with right:
-            st.button("Appointment over", key = appointment['telegram_id'], on_click = db.close_appointment, args = (appointment['telegram_id'],))
+            with st.form(key = str(appointment['telegram_id'])):
+                dr_input = st.text_input(label = "Enter the patient presciption", placeholder = "Enter here")
+                submitted = st.form_submit_button("Submit")
+
+                if submitted and dr_input != "":
+                    appointment_over(appointment['telegram_id'], dr_input)
+            # st.button("Appointment over", key = appointment['telegram_id'], on_click = db.close_appointment, args = (appointment['telegram_id'],))
