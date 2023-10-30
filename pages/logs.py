efb0d8789c5857175ff4b38c620a4dae3ecdf0bf
Robin Roy <robinroy.work@gmail.com>
2023-10-31 04:02:22 +0530

    Not sure of the issue on logs.py and db.py, appparently running the bot.py along with everything seems to fix the issue. This is a working code on my machine. Not sure exactly yet

--- pages/logs.py
+++ pages/logs.py
@@ -1,3 +1,14 @@
+# import streamlit as st
+
+# import db
+
+# with st.form("Input Form"):
+#     search = st.text_input(label = "Enter the register number of student")
+#     submitted = st.form_submit_button("Search")
+
+#     if submitted and search != "":
+#         pass
+
 import streamlit as st
 
 import db
@@ -7,4 +18,16 @@ with st.form("Input Form"):
     submitted = st.form_submit_button("Search")
 
     if submitted and search != "":
-        pass
+        patient_data = db.get_patient_by_reg_no(search.upper())
+        st.markdown(f"""```
+Name: {patient_data['name']}
+Registration Number: {patient_data['reg_no']}
+Age: {patient_data['age']}
+Sex: {patient_data['sex']}
+Room Number: {patient_data['block']} - {patient_data['room_no']}
+Phone Number: {patient_data['phone_no']}
+```""")
+
+        # Populating a datafram table with all previous consultations history
+        data = {"time": patient_data['consultations'][0]['time'], "symptoms": patient_data['consultations'][0]['symptoms'], "prescription": patient_data['consultations'][0]['prescription']}
+        print(data)
