1c0db9d28d14d63831375bd72757a7c230069740
harish.rajkumar2022@vitstudent.ac.in <harish.rajkumar2022@vitstudent.ac.in>
2023-10-30 13:35:23 +0530

    Add register patient feature

--- db.py
+++ db.py
@@ -10,12 +10,39 @@ import os
 load_dotenv()
 client = MongoClient(os.getenv("ATLAS_URI"), server_api=ServerApi('1'))
 db = client[os.getenv("DB_NAME")]
-consultations_collection = db["consultations"]
+patients_collection = db["patients"]
 
 
 # FUNCTIONS
-def get_consultations():
-    return consultations_collection.find({})
+def create_patient(telegram_id: int) -> None:
+    new_patient = {
+        "telegram_id": telegram_id,
+        "has_registered": False,
+        "name": None,
+        "age": None,
+        "sex": None,
+        "reg_no": None,
+        "block": None,
+        "room_no": None,
+        "consultations": []
+    }
 
-for consultation in get_consultations():
-    print(consultation)
+    patients_collection.insert_one(new_patient)
+
+def patient_has_registered(telegram_id: int) -> bool:
+    patient = patients_collection.find_one({"telegram_id": telegram_id})
+    return patient["has_registered"]
+
+def register_patient(telegram_id: int, name: str, age: int, sex: str, reg_no: str, block: str, room_no: int) -> None:
+    patients_collection.update_one(
+        {"telegram_id": telegram_id},
+        {"$set": {
+            "has_registered": True,
+            "name": name,
+            "age": age,
+            "sex": sex,
+            "reg_no": reg_no,
+            "block": block,
+            "room_no": room_no
+        }}
+    )
