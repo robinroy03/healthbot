a020edc9f34c25fb52a93eb380f47e9de7d731bf
harish.rajkumar2022@vitstudent.ac.in <harish.rajkumar2022@vitstudent.ac.in>
2023-10-31 05:09:39 +0530

    Add search logs feature

--- pages/logs.py
+++ pages/logs.py
@@ -1,25 +1,17 @@
-# import streamlit as st
-
-# import db
-
-# with st.form("Input Form"):
-#     search = st.text_input(label = "Enter the register number of student")
-#     submitted = st.form_submit_button("Search")
-
-#     if submitted and search != "":
-#         pass
-
 import streamlit as st
 
 import db
 
+import pandas as pd
+
+
 with st.form("Input Form"):
-    search = st.text_input(label = "Enter the register number of student")
+    search = st.text_input(label = "Enter the register number of the patient: ")
     submitted = st.form_submit_button("Search")
 
-    if submitted and search != "":
-        patient_data = db.get_patient_by_reg_no(search.upper())
-        st.markdown(f"""```
+if submitted and search != "":
+    patient_data = db.get_patient_by_reg_no(search.upper())
+    st.markdown(f"""```
 Name: {patient_data['name']}
 Registration Number: {patient_data['reg_no']}
 Age: {patient_data['age']}
@@ -28,6 +20,31 @@ Room Number: {patient_data['block']} - {patient_data['room_no']}
 Phone Number: {patient_data['phone_no']}
 ```""")
 
-        # Populating a datafram table with all previous consultations history
-        data = {"time": patient_data['consultations'][0]['time'], "symptoms": patient_data['consultations'][0]['symptoms'], "prescription": patient_data['consultations'][0]['prescription']}
-        print(data)
+    # Populating a datafram table with all previous consultations history
+    for consultation in patient_data["consultations"]:
+        time = consultation["time"]
+
+        consultation["symptoms"] = ", ".join(consultation["symptoms"])
+
+        prescription_strings = []
+        for prescription in consultation["prescription"]:
+            name = prescription['name']
+            days = prescription['days']
+            timings = prescription['timings']
+            
+            # Convert True and False values to 'O' and 'X'
+            timings_str = '-'.join(['O' if t else 'X' for t in timings])
+            
+            prescription_string = f"{name} {days}x[{timings_str}]"
+            prescription_strings.append(prescription_string)
+
+        result = ', '.join(prescription_strings)
+
+        consultation["prescription"] = result
+
+    df = pd.DataFrame(patient_data["consultations"])
+    st.dataframe(
+        df[["time", "symptoms", "prescription"]],
+        width=1000,
+        hide_index=True
+    )
