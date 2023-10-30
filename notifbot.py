f166721b6fa3a53ba94f60a1cbe483d8abf13d66
harish.rajkumar2022@vitstudent.ac.in <harish.rajkumar2022@vitstudent.ac.in>
2023-10-31 00:46:30 +0530

    Clean up reply messages

--- notifbot.py
+++ notifbot.py
@@ -15,15 +15,16 @@ db.get_first_appointment() => get the top 5 people to send notifications to
 
 def send_queue_notifications():
     """
-    Fetch the queue people from the db and send notification for the first 5
+    Fetch the queue people from the db and send notification to the next person in queue
     """    
     load_dotenv()
     bot = telebot.TeleBot(os.getenv("NOTIF_BOT_TOKEN"), parse_mode = None)
     for user in db.get_first_appointment():
-        bot.send_message(user['telegram_id'], "You can come now")
+        bot.send_message(user['telegram_id'], "You can meet the doctor now")
 
 def send_message_to_warden(telegram_id: int):
     load_dotenv()
     bot = telebot.TeleBot(os.getenv("NOTIF_BOT_TOKEN"), parse_mode = None)
     # hardcoding Harish's telegram ID => he is the warden here for testing 
-    bot.send_message(5454943365, f"Student {db.get_patient(telegram_id).values()} is sick and needs food to his room")
+    patient = db.get_patient(telegram_id)
+    bot.send_message(5454943365, f'Student {patient["name"]} from room {patient["room_no"]} is sick and need food sent to their room')
