a603aa82491f44a8dc4f0da773ba09191d203f32
harish.rajkumar2022@vitstudent.ac.in <harish.rajkumar2022@vitstudent.ac.in>
2023-10-30 11:59:52 +0530

    Set up db.py

--- db.py
+++ db.py
@@ -1,2 +1,21 @@
-def is_registered(telegram_id: int) -> bool:
-    return True     # for now
+# IMPORTS
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from dotenv import load_dotenv
+import os
+
+
+# SETUP
+load_dotenv()
+client = MongoClient(os.getenv("ATLAS_URI"), server_api=ServerApi('1'))
+db = client[os.getenv("DB_NAME")]
+consultations_collection = db["consultations"]
+
+
+# FUNCTIONS
+def get_consultations():
+    return consultations_collection.find({})
+
+for consultation in get_consultations():
+    print(consultation)
