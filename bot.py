e9de1264b7dd74d8c54de0838e28a783d651fb76
harish.rajkumar2022@vitstudent.ac.in <harish.rajkumar2022@vitstudent.ac.in>
2023-11-02 22:23:26 +0530

    Clean up bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# IMPORTS
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, MenuButtonWebApp
 
@@ -7,31 +8,60 @@ import os
 import db
 import notifbot
 
+
+# SETUP
 load_dotenv()
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"), parse_mode = None)
 
-"""
-Helper functions in the db
-
-patient_exists(telegram_id: int) => check if the user with the given id is registered in the database
-
-# not now for the MVP
-get_current_state(telegram_id: int) => get the current state of the user, right now the plan is it to be 
-['registration', 'started'], ['registration', 'name'] (name is to be entered), ['registration', 'regno'], ['registration', 'phone'],
-['registration', 'vitmail'], ['registration', 'hostel block'], ['registration', 'room number']
-
-# for the mvp
-patient_has_registered(telegram_id: int) => [true, false]
-register_patient(message.text: str) => to store the user acc details for new registration
-"""
-
-ACTIONS = ['Schedule a doctor\'s appointment', 'Call Ambulance', 'Schedule a counsellor appointment', 'Deliver food to room']
+ACTIONS = ["Schedule a doctor\'s appointment", "Call Ambulance", "Schedule a counsellor appointment", "Deliver food to room"]
 markup = ReplyKeyboardMarkup(row_width=1)
 for i in ACTIONS:
     markup.add(KeyboardButton(i))
 
+
+# HELPER FUNCTION
+def doctor_appointment(message: telebot.types.Message) -> None:
+    '''
+    Doctor appointment feature
+    '''
+    bot.reply_to(message, "Done! Your appointment has been scheduled\nHead over to https://t.me/HealthNotificationBot to get notified when the doctor is ready to see you")
+    db.create_appointment(message.chat.id)
+
+def deliver_food_to_room(message: telebot.types.Message) -> None:
+    '''
+    Food delivery feature
+    '''
+    if db.is_patient_sick(message.chat.id):
+        bot.reply_to(message, "Done! food will be sent to your room soon :)")
+        notifbot.send_food_req_notif_to_warden(message.chat.id)
+    else:
+        bot.reply_to(message, "Sorry, you can't avail this feature when you're not sick")
+
+def call_ambulance(message: telebot.types.Message) -> None:
+    '''
+    Call ambulance feature
+    '''
+    bot.reply_to(message, "Done! We'll share this information to the authorities and help will arrive soon")
+    if not db.appointment_exists(message.chat.id):
+        doctor_appointment(message)
+    else:
+        bot.reply_to(message, "You already have an appointment scheduled")    
+    notifbot.send_ambulance_notif(message.chat.id)
+
+def counsellor_appointment(message: telebot.types.Message) -> None:
+    '''
+    Counsellor appointment feature
+    '''
+    bot.reply_to(message, "Done! Your appointment has been scheduled")
+    notifbot.send_appointment_notif_to_counsellor(message.chat.id)
+
+
+# BOT COMMANDS
 @bot.message_handler(commands = ['start'])
-def start(message):
+def start(message: telebot.types.Message) -> None:
+    '''
+    Start command
+    '''
     if not db.patient_exists(message.chat.id):
         db.create_patient(message.chat.id)
         bot.reply_to(message, "Please register yourself using /register")
@@ -39,19 +69,21 @@ def start(message):
         bot.reply_to(message, "Welcome to healthbot, we are here to help you out :)", reply_markup = markup)
 
 @bot.message_handler(commands = ['register'])
-def register(message):
+def register(message: telebot.types.Message) -> None:
+    '''
+    Register command
+    '''
     if db.patient_has_registered(message.chat.id):
         bot.reply_to(message, "You are already registed. You do not have to register again", reply_markup = markup)
     else:
         bot.reply_to(message, '''Enter your details in the following format:\nName\nAge\nSex [M/F]\nRegistration Number\nHostel Block [A/B/C/D1/D2]\nRoom Number\nPhone Number''')
 
 @bot.message_handler(func = lambda message: True)
-def router(message):
-    """
+def router(message: telebot.types.Message):
+    '''
     This function routes the text messages appropriately - acts as a state machine
-    """
+    '''
     if not db.patient_has_registered(message.chat.id):
-        # this response is then the name, reg, phone thing .., so plug it into db
         fields = message.text.split("\n")
         db.register_patient(message.chat.id, fields[0], int(fields[1]), fields[2], fields[3], fields[4], int(fields[5]), int(fields[6]))
         bot.reply_to(message, "You are already registed, click on the button menu to continue", reply_markup = markup)
@@ -67,29 +99,5 @@ def router(message):
     elif message.text == "Schedule a counsellor appointment":
         counsellor_appointment(message)
 
-def doctor_appointment(message):
-    bot.reply_to(message, "Done! Your appointment has been scheduled\nHead over to https://t.me/HealthNotificationBot to get notified when the doctor is ready to see you")
-    db.create_appointment(message.chat.id)
-
-def deliver_food_to_room(message):
-    if db.is_patient_sick(message.chat.id):
-        bot.reply_to(message, "Done! We'll send food to your room soon :)")
-        notifbot.send_food_req_notif_to_warden(message.chat.id)
-    else:
-        bot.reply_to(message, "Sorry, you can't avail this feature when you're not sick")
-
-def call_ambulance(message):
-    # TODO: Penalize those who spam
-
-    bot.reply_to(message, "Done! We'll share this information to the authorities and they'll let you know soon")
-    if not db.appointment_exists(message.chat.id):
-        doctor_appointment(message)
-    else:
-        bot.reply_to(message, "You already have an appointment scheduled")    
-    notifbot.send_ambulance_notif(message.chat.id)
-
-def counsellor_appointment(message):
-    bot.reply_to(message, "Done! Your appointment has been scheduled")
-    notifbot.send_appointment_notif_to_counsellor(message.chat.id)
-
-bot.infinity_polling()
+if __name__ == "__main__":
+    bot.infinity_polling()
